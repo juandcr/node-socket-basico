2284262912e6570abdf94399583e22d2e4a7fdbe
Juan <iktan.lol@gmail.om>
2019-01-09 21:02:54 -0600

    Primer Comit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,19 @@
+var socket = io();
+//Escuchar 
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+socket.on('disconnect', function() {
+    console.log('Perdimos conexión con el servidor');
+});
+// Enviar información
+socket.emit('enviarMensaje', {
+    usuario: 'Juan',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('respuesta server', resp);
+});
+// Escuchar información
+socket.on('enviarMensaje', function(mensaje) {
+    console.log(mensaje);
+})
